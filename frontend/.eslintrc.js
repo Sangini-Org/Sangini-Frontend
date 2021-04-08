6e71b49d7c656d828cbfc7ca089a66c4dd2dd740
Devansh Chaubey <devkumar5436@gmail.com>
2021-04-08 12:43:20 +0530

    pre-commit hooks config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,7 +1,52 @@
 module.exports = {
-    extends: ['airbnb'],
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    parserOptions: {
+      ecmaVersion: 6,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+        modules: true,
+      },
+    },
+    extends: [
+      'plugin:prettier/recommended',
+      'plugin:import/errors',
+      'plugin:import/warnings',
+      'plugin:import/typescript',
+      // TODO: fix accessability issues in one go
+      // "plugin:jsx-a11y/recommended",
+      'plugin:react/recommended',
+    ],
     rules: {
-      'react/jsx-filename-extension': 'off',
-      //You can override any rules you want
+      'react/prop-types': 0,
+      'import/no-unresolved': [2, { commonjs: true, amd: true }],
+      'import/no-named-as-default': 0,
+      'import/no-duplicates': 0,
+      'import/named': 0,
+      'react/no-unescaped-entities': 0,
+      'react/no-children-prop': 0,
+    },
+    plugins: ['react-hooks', 'import'],
+    env: {
+      browser: true,
+      node: true,
+    },
+    settings: {
+      // Apply special parsing for TypeScript files
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+      },
+      // Append 'ts' extensions to Airbnb 'import/resolver' setting
+      'import/resolver': {
+        node: {
+          extensions: ['.tsx', '.ts'],
+        },
+      },
+      // Append 'ts' extensions to Airbnb 'import/extensions' setting
+      'import/extensions': ['.tsx', '.ts'],
+      react: {
+        version: 'detect',
+      },
+      'import/ignore': ['.ts'],
     },
   };
