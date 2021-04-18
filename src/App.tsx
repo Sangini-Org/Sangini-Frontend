dbf2a453b41601ca7f46f9bb288464c8b54729ea
MeghalBisht <mailtomeghu25@gmail.com>
2021-04-18 23:52:41 +0530

    login page added

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import './App.css';
 import theme from '../src/theme';
-import SocialLogin from './Component/SocialLogin/SocialLogin';
-import Signup from './Component/SignUp/Signup';
+import Login from './Component/Login/Login';
+
 function App() {
   // const [state, setstate] = useState()
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Signup />
+        <Login />
       </div>
     </ThemeProvider>
   );
--- src/Component/Login/Login.tsx
+++ src/Component/Login/Login.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import { Button, Link, TextField, Switch, FormGroup, FormControlLabel, Box } from '@material-ui/core';
+import React, { useState } from 'react';
+import styles from './Login.module.css';
 
-export default function Login() {
+export default function Signup() {
+  const [checked, setChecked] = React.useState(false);
+
+  const toggleChecked = () => {
+    setChecked((prev) => !prev);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className={`text-center flex flex-center flex-col ${styles.baseContainer}`}>
+      <h1 className={styles.title}>Login</h1>
+      <div className={styles.loginContainer}>
+        <form>
+          <TextField variant="outlined" margin="normal" id="outlined-basic" placeholder="Username / Email" fullWidth />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            id="outlined-password-input"
+            type="password"
+            placeholder="Password"
+            fullWidth
+          />
+          <Box mt={2} mb={2} className="flex flex-space-bw">
+            <FormGroup>
+              <FormControlLabel
+                control={<Switch color="primary" size="small" checked={checked} onChange={toggleChecked} />}
+                label="Remember me"
+              />
+            </FormGroup>
+            <Link href="#">Forgot Password?</Link>
+          </Box>
+          <Button className={`btn ${styles.loginBtn}`} type="submit" variant="contained" fullWidth>
+            Login
+          </Button>
+        </form>
+        <div className="text-muted">
+          <span>Don't have an account? &nbsp;</span>
+          <Link href="#">Sign Up</Link>
+        </div>
+      </div>
     </div>
   );
 }
