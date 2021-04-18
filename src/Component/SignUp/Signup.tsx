a70b831533f5e9b8cd8075b009695f7ff37b2609
MeghalBisht <mailtomeghu25@gmail.com>
2021-04-18 23:04:58 +0530

    finalize

--- src/Component/SignUp/Signup.tsx
+++ src/Component/SignUp/Signup.tsx
@@ -4,33 +4,29 @@ import styles from './SignUp.module.css';
 
 export default function Signup() {
   return (
-    <>
-      <div className={styles.basecontainer}>
-        <div className={styles.title}>SignUp</div>
-        <div className={styles.signupcontainer}>
-          <form>
-            <TextField variant="outlined" margin="normal" id="outlined-basic" placeholder="Username" fullWidth />
-            <TextField variant="outlined" margin="normal" id="email" placeholder="Email Address" fullWidth />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              id="outlined-password-input"
-              type="password"
-              placeholder="Create Password"
-              fullWidth
-            />
-            <Button className={styles.btn} type="submit" variant="contained" fullWidth>
-              Register
-            </Button>
-          </form>
-          <div className={styles.typo}>
-            You have an Account? &nbsp;
-            <Link href="#" variant="body1">
-              Sign In
-            </Link>
-          </div>
+    <div className={`text-center flex flex-center flex-col ${styles.baseContainer}`}>
+      <h1 className={styles.title}>SignUp</h1>
+      <div className={styles.signupContainer}>
+        <form>
+          <TextField variant="outlined" margin="normal" id="outlined-basic" placeholder="Username" fullWidth />
+          <TextField variant="outlined" margin="normal" id="email" placeholder="Email Address" fullWidth />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            id="outlined-password-input"
+            type="password"
+            placeholder="Create Password"
+            fullWidth
+          />
+          <Button className={`btn ${styles.signupBtn}`} type="submit" variant="contained" fullWidth>
+            Register
+          </Button>
+        </form>
+        <div className="text-muted">
+          <span>Already have an Account? &nbsp;</span>
+          <Link href="#">Sign In</Link>
         </div>
       </div>
-    </>
+    </div>
   );
 }
