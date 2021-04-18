0120ca253a735b8e28e9b940a1e55cac4b214051
MeghalBisht <mailtomeghu25@gmail.com>
2021-04-18 23:59:19 +0530

    f'

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import './App.css';
 import theme from '../src/theme';
 import Login from './Component/Login/Login';
+import SignUp from './Component/SignUp/SignUp';
 
 function App() {
   // const [state, setstate] = useState()
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Login />
+        <SignUp />
       </div>
     </ThemeProvider>
   );
