a6935d06bc839c049d0f9a81132755324e372123
MeghalBisht <mailtomeghu25@gmail.com>
2021-04-24 20:34:41 +0530

    add tailwind as css framework, remove material ui

--- src/Component/SocialLogin/SocialLogin.tsx
+++ src/Component/SocialLogin/SocialLogin.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import styles from './SociaLogin.module.css';
-import CancelIcon from '@material-ui/icons/Cancel';
-import { Button } from '@material-ui/core';
 
 export default function SocialLogin() {
   return (
-    <div className="h-100 flex flex-col flex-center">
-      <CancelIcon fontSize="large" className="cancel text" />
+    <div className="h-full flex flex-col flex-center">
       <div className="flex flex-col flex-center">
-        <h1 className={styles.sanginiText}>Sangini</h1>
-        <h3 className="text">Signup with social media</h3>
-        <div className="flex flex-col flex-center">
-          <Button className={`btn ${styles.fb}`} variant="contained">
-            Continue with Facebook
-          </Button>
-          <Button className={`btn ${styles.google}`} variant="contained">
-            Continue with Google
-          </Button>
+        <h1 className={`font-bold ${styles.sanginiText}`}>Sangini</h1>
+        <h3 className="font-normal text-xl text mb-6">Signup with social media</h3>
+        <div className="w-full flex flex-col flex-center">
+          <button className={`rounded w-full uppercase m-1 btn ${styles.fb}`}>Continue with Facebook</button>
+          <button className={`rounded w-full uppercase m-1 btn ${styles.google}`}>Continue with Google</button>
         </div>
       </div>
     </div>
