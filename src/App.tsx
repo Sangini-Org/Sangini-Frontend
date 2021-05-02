fc2cf381d0c863605ca4b8fc7975af1f12d73fec
MeghalBisht <mailtomeghu25@gmail.com>
2021-05-03 03:46:19 +0530

    f

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Landing from './Component/Landing/Landing';
 import ResetPassword from './Component/ResetPassword/ResetPassword';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { useAuthStore } from '../src/stores/useAuthStore';
+import EditProfile from './Component/EditProfile/EditProfile';
 
 function App() {
   // const [state, setstate] = useState()
@@ -24,6 +25,7 @@ function App() {
         {userId ? (
           <div>
             <Route path="/profile" exact component={Profile} />
+            <Route path="/profile/edit" exact component={EditProfile} />
           </div>
         ) : (
           <Redirect to="/" />
