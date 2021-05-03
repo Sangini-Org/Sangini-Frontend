48cea8e94068e5c1c3ac34a73b73cb8552db83c9
Harshit <66195325+hss23@users.noreply.github.com>
2021-05-04 01:11:28 +0530

    fix: Navbar icon-update (#18)
    Co-authored-by: MeghalBisht <mailtomeghu25@gmail.com>

--- src/Component/NavBar/NavBar.tsx
+++ src/Component/NavBar/NavBar.tsx
@@ -1,37 +1,52 @@
-import React from 'react';
-import { ExploreIcon, HeartIcon, NotificationIcon, ProfileIcon } from '../Utils/Svg/Svgs';
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { IconContext } from 'react-icons';
+import { FaSearch, FaBell, FaHeart, FaUser } from 'react-icons/fa';
 function NavBar() {
+  const [activeMenuItem, setActiveMenuItem] = useState(0);
   return (
-    <div className="bottom-0 bg-purple-300 w-full absolute md:w-36 md:h-full md:m-auto md:rounded-r-lg">
-      <nav className="tabs flex justify-center text-xs md:flex-col md:items-center md:py-28">
-        <button
-          data-target="panel-1"
-          className="tab active w-1/4 flex flex-col items-center justify-center text-gray-600 py-4 md:py-6
-            hover:text-blue-500 focus:outline-none text-blue-500 font-medium border-blue-500">
-          <ExploreIcon />
-          <span className="mt-1.5">Explore</span>
-        </button>
-        <button
-          data-target="panel-2"
-          className="tab w-1/4 flex flex-col items-center justify-center ext-gray-600 py-4 md:py-6 hover:text-blue-500 focus:outline-none">
-          <HeartIcon />
-          <span className="mt-1.5">Heart</span>
-        </button>
-        <button
-          data-target="panel-3"
-          className="tab w-1/4 flex flex-col items-center justify-center text-gray-600 py-4 md:py-6 hover:text-blue-500 focus:outline-none">
-          <NotificationIcon />
-          <span className="mt-1.5">Notification</span>
-        </button>
-        <button
-          data-target="panel-3"
-          className="tab w-1/4 flex flex-col items-center justify-center text-gray-600 py-4 md:py-6 hover:text-blue-500 focus:outline-none">
-          <ProfileIcon />
-          <p className="mt-1.5">Profile</p>
-        </button>
-      </nav>
-    </div>
+    <IconContext.Provider value={{ size: '1.75rem' }}>
+      <div className="bottom-0 bg-purple-300 w-full absolute md:w-36 md:h-full md:m-auto md:rounded-r-lg">
+        <nav className="tabs flex justify-center text-xs md:flex-col md:items-center md:py-28">
+          <button
+            onClick={() => setActiveMenuItem(0)}
+            data-target="panel-1"
+            className={`${
+              activeMenuItem === 0 ? 'bg-purple-400 opacity-70' : ''
+            } w-full py-4 md:py-6 md:m-1.5 flex flex-col items-center justify-center text-gray-600 hover:opacity-70 hover:bg-purple-400`}>
+            <FaSearch className={activeMenuItem === 0 ? 'text-white' : 'text-purple-900'} />
+            <span className={`${activeMenuItem === 0 ? 'text-white' : 'text-purple-900'} mt-1.5`}>Explore</span>
+          </button>
+          <button
+            onClick={() => setActiveMenuItem(1)}
+            data-target="panel-2"
+            className={`${
+              activeMenuItem === 1 ? 'bg-purple-400 opacity-70' : ''
+            } w-full py-4 md:py-6 md:m-1.5 flex flex-col items-center justify-center text-gray-600 hover:opacity-70 hover:bg-purple-400`}>
+            <FaHeart className={activeMenuItem === 1 ? 'text-white' : 'text-purple-900'} />
+            <span className={`${activeMenuItem === 1 ? 'text-white' : 'text-purple-900'} mt-1.5`}>Heart</span>
+          </button>
+          <button
+            onClick={() => setActiveMenuItem(2)}
+            data-target="panel-3"
+            className={`${
+              activeMenuItem === 2 ? 'bg-purple-400 opacity-70' : ''
+            } w-full py-4 md:py-6 md:m-1.5 flex flex-col items-center justify-center text-gray-600 hover:opacity-70 hover:bg-purple-400`}>
+            <FaBell className={activeMenuItem === 2 ? 'text-white' : 'text-purple-900'} />
+            <span className={`${activeMenuItem === 2 ? 'text-white' : 'text-purple-900'} mt-1.5`}>Notification</span>
+          </button>
+          <button
+            onClick={() => setActiveMenuItem(3)}
+            data-target="panel-3"
+            className={`${
+              activeMenuItem === 3 ? 'bg-purple-400 opacity-70' : ''
+            } w-full py-4 md:py-6 md:m-1.5 flex flex-col items-center justify-center text-gray-600 hover:opacity-70 hover:bg-purple-400`}>
+            <FaUser className={activeMenuItem === 3 ? 'text-white' : 'text-purple-900'} />
+            <span className={`${activeMenuItem === 3 ? 'text-white' : 'text-purple-900'} mt-1.5`}>Profile</span>
+          </button>
+        </nav>
+      </div>
+    </IconContext.Provider>
   );
 }
 
