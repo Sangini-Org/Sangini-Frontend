25da3815e871f7a6bc1a954226379058425aa213
MeghalBisht <mailtomeghu25@gmail.com>
2021-05-04 05:52:14 +0530

    fix width

--- src/Component/Dropdown/Dropdown.tsx
+++ src/Component/Dropdown/Dropdown.tsx
@@ -14,7 +14,7 @@ function Dropdown({ dropdown, setDropdown }: dropdownAgs) {
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true">
-      <div className="w-3/4 md:w-1/2 md:mx-4 flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div className="w-full md:w-1/2 md:mx-4 flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
         <div className="w-full max-h-700 overflow-y-scroll min-h-500 static inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all align-middle">
           <AiFillCloseCircle
