97d8d36274b67778dda61067a503d61ee603b99c
MeghalBisht <mailtomeghu25@gmail.com>
2021-05-05 23:30:09 +0530

    f

--- src/Component/Utils/Loaders/Loader.tsx
+++ src/Component/Utils/Loaders/Loader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Loader.module.css';
 export default function Loader1() {
   return (
-    <div className={`${styles.pulseOut} rounded w-full h-full flex justify-center items-center`}>
+    <div className={`${styles.pulseOut} w-full h-full flex justify-center items-center`}>
       <div className={`w-1 h-8 mx-1 bg-green-500`}></div>
       <div className={`w-1 h-8 mx-1 bg-green-500`}></div>
       <div className={`w-1 h-8 mx-1 bg-green-500`}></div>
