08b54540c0eac44f01c15c8bd6396e2ed9b67de7
MeghalBisht <mailtomeghu25@gmail.com>
2021-05-07 03:46:41 +0530

    f

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 
+Sentry.init({
+  dsn: 'https://ab4dcedc1243486ea3186b958af38d4b@o622943.ingest.sentry.io/5752960',
+  autoSessionTracking: true,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
