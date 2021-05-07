215e27dca9186d82616c40127ed87cb6201223a0
Meghal Bisht <mailtomeghu25@gmail.com>
2021-05-07 23:20:20 +0530

    c

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ Sentry.init({
   autoSessionTracking: true,
   integrations: [new Integrations.BrowserTracing()],
 
+  
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
