3d1a2fcb2faf8ea646f52e5ad8befb3e338c4dc4
Devansh Chaubey <devkumar5436@gmail.com>
2021-05-13 01:28:35 +0530

    google oauth testing

--- src/Component/SocialLogin/SocialLogin.tsx
+++ src/Component/SocialLogin/SocialLogin.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './SociaLogin.module.css';
+import axios from 'axios';
 
 export default function SocialLogin() {
+  const [loginUrl, setLoginUrl] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/auth/google/url').then((res) => {
+      setLoginUrl(res.data.data);
+      console.log(res);
+    });
+  }, []);
+  console.log(loginUrl);
+
   return (
     <div className="h-full flex flex-col flex-center">
       <div className={`flex flex-col flex-center ${styles.baseContainer}`}>
@@ -9,7 +20,9 @@ export default function SocialLogin() {
         <h3 className="font-normal text-xl text mb-6">Signup with social media</h3>
         <div className="w-full flex flex-col flex-center">
           <button className={`rounded w-full uppercase m-1 btn ${styles.fb}`}>Continue with Facebook</button>
-          <button className={`rounded w-full uppercase m-1 btn ${styles.google}`}>Continue with Google</button>
+          <a href={loginUrl} className={`rounded w-full uppercase m-1 btn ${styles.google}`}>
+            Continue with Google
+          </a>
         </div>
       </div>
     </div>
