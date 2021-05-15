3dce2a6d8ace62b50a3590b5604956610ba64d15
ranjit <ranjitmaheshshah@gmail.com>
2021-05-15 07:51:46 +0530

    react-google-login

--- src/Component/SocialLogin/SocialLogin.tsx
+++ src/Component/SocialLogin/SocialLogin.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from './SociaLogin.module.css';
 import axios from 'axios';
+import { GoogleLogin } from 'react-google-login';
 
 export default function SocialLogin() {
   const [loginUrl, setLoginUrl] = useState('');
@@ -12,6 +13,16 @@ export default function SocialLogin() {
     });
   }, []);
   console.log(loginUrl);
+  const googleLoggedIn = (response: any) => {
+    console.log(response);
+    axios.post('http://localhost:8080/api/auth/google/url').then((res) => {
+      setLoginUrl(res.data.data);
+      console.log(res);
+    });
+  };
+  const errorGoogle = (response: any) => {
+    console.log(response);
+  };
 
   return (
     <div className="h-full flex flex-col flex-center">
@@ -20,6 +31,13 @@ export default function SocialLogin() {
         <h3 className="font-normal text-xl text mb-6">Signup with social media</h3>
         <div className="w-full flex flex-col flex-center">
           <button className={`rounded w-full uppercase m-1 btn ${styles.fb}`}>Continue with Facebook</button>
+          <GoogleLogin
+            clientId="812836693680-ift7r4mbe93bnlekml89e3t8p1g3g2lj.apps.googleusercontent.com"
+            buttonText="Google Login"
+            onSuccess={googleLoggedIn}
+            onFailure={errorGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
           <a href={loginUrl} className={`rounded w-full uppercase m-1 btn ${styles.google}`}>
             Continue with Google
           </a>
