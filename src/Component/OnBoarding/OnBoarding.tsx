66d69795cedaaf1bae474803751985b8f5325525
MeghalBisht <mailtomeghu25@gmail.com>
2021-05-31 00:18:12 +0530

    f

--- src/Component/OnBoarding/OnBoarding.tsx
+++ src/Component/OnBoarding/OnBoarding.tsx
@@ -1,28 +1,25 @@
 import React from 'react';
-
 import styles from './OnBoarding.module.css';
-import logo from '../../Images/Money.png';
-import listening from '../../Images/Listening.png';
+import logo from '../Utils/Images/Money.png';
+import listening from '../Utils/Images/Listening.png';
 
 export default function OnBoarding() {
   return (
-    <div>
+    <div className={`${styles.landingContainer} mx-auto`}>
       <img className={`block mx-auto mt-20`} src={logo} alt="Logo" />
-      <img className={`block mx-auto mt-8 mb-6`} src={listening} alt="Listening" />
+      <img className={`block mx-auto my-6`} src={listening} alt="Listening" />
 
-      <div className={`${styles.circle} rounded-full fixed -bottom-28 right-4`}></div>
-      <div className={`${styles.content} pb-8 pl-4 mt-24`}>
-        <p className={`${styles.heading} text-white font-bold text-xl`}>
+      <div className={`${styles.overlay} bg-white bg-opacity-10 rounded-full fixed -bottom-28`}></div>
+      <div className="pb-8 pl-4 mt-24">
+        <p className="text-white font-bold text-xl">
           Connect with people with <br /> same music taste.
         </p>
 
-        <p className={`${styles.subHeading} mt-4 primary-txt font-normal text-sm`}>
+        <p className="mt-4 primary-txt font-normal text-sm">
           You can chat and you can express <br /> your feeling as emoji.
         </p>
 
-        <button className={`${styles.explore} py-4 px-12 mt-16 primary-bg text rounded-full border-none`}>
-          <b>Explore</b>
-        </button>
+        <button className="py-4 font-bold px-12 mt-16 primary-bg text rounded-full border-none">Explore</button>
       </div>
     </div>
   );
