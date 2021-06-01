4473381f678050b938eb103788641abbdbd62828
MeghalBisht <mailtomeghu25@gmail.com>
2021-06-01 13:09:54 +0530

    fix

--- src/Component/Login/Login.tsx
+++ src/Component/Login/Login.tsx
@@ -6,60 +6,60 @@ import { axiosConfig } from '../../configs/axios';
 import { apiEndPoints } from '../../configs/endpoints';
 import { useAuthStore } from '../../stores/useAuthStore';
 import { toast } from 'react-toastify';
+import money from '../Utils/Images/Money.png';
 import { FaFacebook, FcGoogle, FiCheck, BsFillEyeSlashFill } from 'react-icons/all';
 
 export default function Login() {
   return (
-    <div className={`h-full flex mx-auto ${styles.baseContainer}`}>
-      <div className="w-full mx-3 min-h-1/2 p-7 flex flex-col">
-        <h2 className="flex flex-center h-28 text-white">Sangini</h2>
-        <h1 className={`text-3xl font-medium text-white mb-10`}>Sign in</h1>
-        <div className="mx-1.5">
-          <form>
-            <label htmlFor="" className="text-white">
-              Username or email
-            </label>
-            <div className="w-full relative flex justify-end items-center text-white my-4">
-              <input
-                className={`w-full h-20 px-6 md:h-14 ${styles.input}`}
-                id="outlined-basic"
-                placeholder="Enter your email address"
-                required
-              />
-              <FiCheck className="absolute right-4 text-2xl md:text-3xl" />
-            </div>
-            <label htmlFor="" className="text-white">
-              Password
-            </label>
-            <div className="w-full relative flex justify-end items-center text-white my-4">
-              <input
-                className={`w-full h-20 px-6 md:h-14 ${styles.input}`}
-                id="outlined-password-input"
-                type="password"
-                placeholder="Password"
-                required
-              />
-              <BsFillEyeSlashFill className="absolute right-4 text-2xl md:text-3xl" />
-            </div>
-            <button className={`w-full h-20 my-3 font-bold rounded-2xl md:h-14 ${styles.signupBtn}`} type="submit">
-              Sign in
-            </button>
-          </form>
-          <div className="text-muted text-center">
-            <h2 className={`overflow-hidden my-4 text-white ${styles.bottom}`}>or sign in with</h2>
-            <div className="flex flex-row justify-evenly text-4xl my-8">
-              <Link to="/">
-                <FaFacebook className={`${styles.icon}`} style={{ fill: '#0000FF' }} />
-              </Link>
-              <Link to="/">
-                <FcGoogle className={`${styles.icon}`} />
-              </Link>
-            </div>
-            <span className="active">Don't you have an account ? &nbsp;</span>
-            <Link to="/register" className="active">
-              Sign in now
+    <div className={`h-full flex mx-auto p-3 flex flex-col ${styles.baseContainer}`}>
+      <img src={money} className="w-16 h-auto mx-auto my-10" alt="icon" />
+      <h1 className="text-3xl font-medium text-white mb-8 font-bold px-2">Sign in</h1>
+      <div className="px-1">
+        <form>
+          <label htmlFor="" className="px-1 text-white">
+            Username or email
+          </label>
+          <div className="w-full relative flex justify-end items-center text-white mt-4 mb-8">
+            <input
+              className="text-white w-full px-6 py-4 dark-sec-bg rounded-full"
+              id="outlined-basic"
+              placeholder="Enter your email address"
+              required
+            />
+            <FiCheck className="absolute right-6 text-xl" />
+          </div>
+          <label htmlFor="" className="px-1 text-white">
+            Password
+          </label>
+          <div className="w-full relative flex justify-end items-center text-white mt-4 mb-4">
+            <input
+              className="text-white w-full px-6 py-4 dark-sec-bg rounded-full"
+              id="outlined-password-input"
+              type="password"
+              placeholder="Password"
+              required
+            />
+            <BsFillEyeSlashFill className="absolute right-6 text-xl" />
+          </div>
+          <button className="primary-txt">Forgot Password?</button>
+          <button className="w-full py-5 my-5 font-bold rounded-2xl primary-bg text-xl rounded-full" type="submit">
+            Sign in
+          </button>
+        </form>
+        <div className="px-2 text-muted text-center">
+          <h2 className={`overflow-hidden my-4 text-white ${styles.bottom}`}>or sign in with</h2>
+          <div className="flex flex-row justify-evenly text-4xl my-10">
+            <Link to="/">
+              <FaFacebook className={`w-9 h-9 ${styles.icon}`} style={{ fill: '#4267B2' }} />
+            </Link>
+            <Link to="/">
+              <FcGoogle className={`w-9 h-9 ${styles.icon}`} />
             </Link>
           </div>
+          <span className="text-white">Don't you have an account? &nbsp;</span>
+          <Link to="/register" className="primary-txt">
+            Sign in
+          </Link>
         </div>
       </div>
     </div>
--- src/Component/SignUp/SignUp.tsx
+++ src/Component/SignUp/SignUp.tsx
@@ -6,59 +6,71 @@ import { axiosConfig } from '../../configs/axios';
 import { apiEndPoints } from '../../configs/endpoints';
 import { toast } from 'react-toastify';
 import { FaFacebook, FcGoogle, FiCheck, AiOutlineEye } from 'react-icons/all';
+import money from '../Utils/Images/Money.png';
 
 export default function SignUp() {
   return (
-    <div className={`h-full flex mx-auto ${styles.baseContainer}`}>
-      <div className="w-full mx-3 min-h-1/2 p-7 flex flex-col">
-        <h2 className="flex flex-center h-28 text-white">Sangini</h2>
-        <h1 className={`text-3xl font-medium text-white mb-10`}>Sign up</h1>
-        <div className="mx-1.5">
-          <form>
-            <label htmlFor="" className="text-white">
-              Username or email
-            </label>
-            <div className="w-full relative flex justify-end items-center text-white my-4">
-              <input
-                className={`w-full h-20 px-6 md:h-14 ${styles.input}`}
-                id="outlined-basic"
-                placeholder="Enter your email address"
-                required
-              />
-              <FiCheck className="absolute right-4 text-2xl md:text-3xl" />
-            </div>
-            <label htmlFor="" className="text-white">
-              Password
-            </label>
-            <div className="w-full relative flex justify-end items-center text-white my-4">
-              <input
-                className={`w-full h-20 px-6 md:h-14 ${styles.input}`}
-                id="outlined-password-input"
-                type="password"
-                placeholder="Password"
-                required
-              />
-              <AiOutlineEye className="absolute right-4 text-2xl md:text-3xl" />
-            </div>
-            <button className={`w-full h-20 my-3  font-bold rounded-2xl md:h-14 ${styles.signupBtn}`} type="submit">
-              Sign up
-            </button>
-          </form>
-          <div className="text-muted text-center">
-            <h2 className={`overflow-hidden my-4 text-white ${styles.bottom}`}>or sign up with</h2>
-            <div className="flex flex-row justify-evenly text-4xl my-8">
-              <Link to="/">
-                <FaFacebook className={`${styles.icon}`} style={{ fill: '#0000FF' }} />
-              </Link>
-              <Link to="/">
-                <FcGoogle className={`${styles.icon}`} />
-              </Link>
-            </div>
-            <span className="active">You have an account ? &nbsp;</span>
-            <Link to="/login" className="active">
-              Sign in now
+    <div className={`h-full flex mx-auto p-3 flex flex-col ${styles.baseContainer}`}>
+      <img src={money} className="w-16 h-auto mx-auto my-10" alt="icon" />
+      <h1 className="text-3xl font-medium text-white mb-8 font-bold px-2">Sign up</h1>
+      <div className="px-1">
+        <form>
+          <label htmlFor="" className="px-1 text-white">
+            Username or email
+          </label>
+          <div className="w-full relative flex justify-end items-center text-white mt-4 mb-8">
+            <input
+              className="text-white w-full px-6 py-4 dark-sec-bg rounded-full"
+              id="outlined-basic"
+              placeholder="Enter your email address"
+              required
+            />
+            <FiCheck className="absolute right-6 text-xl" />
+          </div>
+          <label htmlFor="" className="text-white">
+            Password
+          </label>
+          <div className="w-full relative flex justify-end items-center text-white mt-4 mb-4">
+            <input
+              className="text-white w-full px-6 py-4 dark-sec-bg rounded-full"
+              id="outlined-password-input"
+              type="password"
+              placeholder="Password"
+              required
+            />
+            <AiOutlineEye className="absolute right-6 text-xl" />
+          </div>
+          <label htmlFor="" className="text-white">
+            Confirm Password
+          </label>
+          <div className="w-full relative flex justify-end items-center text-white mt-4 mb-4">
+            <input
+              className="text-white w-full px-6 py-4 dark-sec-bg rounded-full"
+              id="outlined-password-input"
+              type="password"
+              placeholder="Re enter password"
+              required
+            />
+            <AiOutlineEye className="absolute right-6 text-xl" />
+          </div>
+          <button className="w-full py-5 my-5 font-bold rounded-2xl primary-bg text-xl rounded-full" type="submit">
+            Sign up
+          </button>
+        </form>
+        <div className="px-2 text-muted text-center">
+          <h2 className={`overflow-hidden my-4 text-white ${styles.bottom}`}>or sign up with</h2>
+          <div className="flex flex-row justify-evenly text-4xl my-10">
+            <Link to="/">
+              <FaFacebook className={`w-9 h-9 ${styles.icon}`} style={{ fill: '#0000FF' }} />
+            </Link>
+            <Link to="/">
+              <FcGoogle className={`w-9 h-9 ${styles.icon}`} />
             </Link>
           </div>
+          <span className="text-white">You have an account ? &nbsp;</span>
+          <Link to="/login" className="primary-txt">
+            Sign in
+          </Link>
         </div>
       </div>
     </div>
