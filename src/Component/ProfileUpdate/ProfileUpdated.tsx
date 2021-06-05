224bfcd0356ff23bcd4ac38f0b2ad8709e962f3a
MeghalBisht <mailtomeghu25@gmail.com>
2021-06-05 19:46:58 +0530

    f

--- src/Component/ProfileUpdate/ProfileUpdated.tsx
+++ src/Component/ProfileUpdate/ProfileUpdated.tsx
@@ -3,10 +3,10 @@ import { TickLottie } from '../Utils/LottieAnimation/LottiePlayer';
 
 const ProfileUpdated = () => {
   return (
-    <div className={`flex flex-center justify-center flex-col m-auto text-white`}>
+    <div className={`flex flex-center justify-center flex-col py-10 md:py-24 m-auto text-white`}>
       <TickLottie />
-      <p className="p-12 text-center">Your Profile has been updated successfully</p>
-      <button className="py-3 primary-bg rounded-full w-80 mt-20">Back</button>
+      <p className="pb-12 pt--12 text-center">Your profile has been updated successfully!</p>
+      <button className="py-3 primary-bg rounded-full w-80 mt-10">Back</button>
     </div>
   );
 };
