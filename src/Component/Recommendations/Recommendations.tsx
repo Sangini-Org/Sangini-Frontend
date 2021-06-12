9184438b19ea56af34343ea15b3fa9e76c4ff960
Meghal Bisht <mailtomeghu25@gmail.com>
2021-06-12 16:32:11 +0530

    add user tags on recommendation screen (#53)

--- src/Component/Recommendations/Recommendations.tsx
+++ src/Component/Recommendations/Recommendations.tsx
@@ -3,6 +3,9 @@ import styles from './Recommendations.module.css';
 import explore from '../Utils/Images/explore.png';
 import { FilterBox } from '../Utils/Filter/Filter';
 import { BsFilter, AiFillPlayCircle, MdLocationOn } from 'react-icons/all';
+import { Hobbies } from '../Utils/Dictionary/Hobbies';
+import { getRandomColor } from '../Utils/Functions/RandomColor';
+
 function Recommendations() {
   const [request, setRequest] = useState(true);
   const [filter, setfilter] = useState(false);
@@ -14,7 +17,7 @@ function Recommendations() {
         <h1 className="font-bold px-8">Recommendations</h1>
         <BsFilter className="cursor-pointer align-right" onClick={() => setfilter(true)} />
       </div>
-      <div className={`dark-sec-bg ${styles.userCard} p-6 mb-4 rounded-2xl`}>
+      <div className={`dark-sec-bg ${styles.userCard} p-6 pb-3 mb-4 rounded-2xl`}>
         <div className="flex flex-col justify-center align-center">
           <img src={explore} className="rounded-2xl" alt="explore_user" />
           <p className="primary-bg rounded-full h-14 w-14 flex flex-center flex-col text-xs p-1 -mt-10 -ml-4">
@@ -40,6 +43,20 @@ function Recommendations() {
               </span>
             </p>
           </div>
+        </div>
+        <div className="flex flex-center flex-wrap mt-4">
+          {Hobbies.slice(0, 10).map((state: String, i: number) => {
+            let clr = getRandomColor();
+            return (
+              <p
+                key={i}
+                style={{ backgroundColor: `${clr}`, color: `white` }}
+                className={`font-bold cursor-pointer text-xs rounded-md px-2 py-1 m-1 opacity-80 hover:opacity-100`}
+                id={`hobby${i}`}>
+                {state}
+              </p>
+            );
+          })}
         </div>
       </div>
       <div className="text-md flex flex-col">
