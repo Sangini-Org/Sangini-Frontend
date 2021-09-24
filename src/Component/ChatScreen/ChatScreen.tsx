e87eddeb8feadc545fbd1280da92a74a4f10dc8d
devansh5 <devkumar5436@gmail.com>
2021-09-25 00:12:45 +0530

    chat system intial

--- src/Component/ChatScreen/ChatScreen.tsx
+++ src/Component/ChatScreen/ChatScreen.tsx
@@ -1,30 +1,34 @@
 import React, { useEffect } from 'react';
-import { w3cwebsocket as W3CWebSocket } from 'websocket';
-
-const client = new W3CWebSocket('ws://localhost:8000');
+import { useAuthStore } from '../../stores/useAuthStore';
+import socket from '../../socket';
 
 export default function ChatScreen() {
+  const userId = useAuthStore((state) => state.userId);
   useEffect(() => {
-    client.onopen = () => {
-      console.log('connected');
-    };
-    client.onmessage = (message: any) => {
-      const data = JSON.parse(message.data);
-      console.log('reply', data);
-    };
+    socket.auth = { userId };
+    socket.connect();
+    socket.on('private message', ({ content, from }) => {
+      console.log(content, from);
+    });
   });
-
-  const checkSend = (value: string) => {
-    client.send(
-      JSON.stringify({
-        type: 'message',
-        msg: value,
-      })
-    );
+  const sendMessage = (message: string) => {
+    let users = ['fb65719e-c077-4bbf-8b86-04bdcea34900', 'cf1e8aa9-34b2-4c6f-8965-c84375a9f933'];
+    socket.emit('private message', {
+      message,
+      to: users.filter((user) => user != userId).pop(),
+    });
   };
   return (
-    <div className="bg-white h-3/4">
-      <button onClick={() => checkSend('hello devansh')}>Check message sent</button>
+    <div className="text-white">
+      <input className={`outline-none rounded-md dark-sec-bg py-1 mb-5 mt-1 w-full`} />
+      <button>Check message sent</button>
+      <button
+        className="font-semibold rounded-md my-4 py-1.5 px-12 dark-sec-bg"
+        onClick={() => {
+          sendMessage('hello Connection');
+        }}>
+        Send
+      </button>
       <input type="text" />
     </div>
   );
